596759c983a83cf1b1e0a9c6ab9db541ed5547d6
AikaOnimura <aika.onimura@gmail.com>
2020-09-28 10:34:07 +0000

    [Add]4-3 画像投稿機能/HTML・コントローラ・モデルに画像アップ用メソッドの追加

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -1,14 +1,25 @@
 class PostImagesController < ApplicationController #画像投稿用のコントローラ
-  
+
     def new
+        @post_image = PostImage.new #画像投稿用空のインスタンス
     end
 
     def create #保存
+        @post_image = PostImage.new(post_image_params)
+        @post_image.user_id = current_user.id
+        @post_image.save
+        redirect_to #投稿一覧
     end
 
     def index
     end
 
     def show
+    end
+
+    private #投稿データのストロングパラメータ設定
+
+    def post_image_params
+        params.require(:post_image).permit(:shop_name, :image, :caption)
     end
 end
--- app/models/post_image.rb
+++ app/models/post_image.rb
@@ -1,3 +1,4 @@
-class PostImage < 
+class PostImage < ApplicationRecord
     belongs_to :user #PostImageモデルに関連付けられるのは、1つのUserモデル
+    attachment :image # ここを追加（_idは含めません）
 end
