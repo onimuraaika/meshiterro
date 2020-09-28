f3b37cc3ac74c60f364753f7a00a1943b9563e0a
AikaOnimura <aika.onimura@gmail.com>
2020-09-28 03:14:41 +0000

    [Add]4-2 devise機能を使ってユーザー登録機能・ログイン機能を追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
-class User < ApplicationRecord
+class User < ApplicationRecord #deviseで利用する機能が記述されている
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
+  devise :database_authenticatable, :registerable, #「:」の後ろがdeviseの機能名
          :recoverable, :rememberable, :validatable
 end
