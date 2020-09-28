c9adcb9fc70d55dbc1e828385b22df5a662b62cf
AikaOnimura <aika.onimura@gmail.com>
2020-09-28 14:50:13 +0000

    [Add]4-4 詳細画面にコメント投稿するためのコントローラを作成、ルーティングの設定

---
+++ app/controllers/post_comments_controller.rb
@@ -0,0 +1,9 @@
+#投稿画像の詳細画面（/post_image/:id/）にコメント投稿機能を実装するためのコントローラ
+class PostCommentsController < ApplicationController
+  
+    def create #コメント投稿を保存
+    end
+
+    def destroy #コメント投稿を
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
     root 'post_images#index' #ログイン認証が成功後、投稿画像一覧画面が表示されるように設定
     devise_for :users #deviseを使用する際にURLとしてusersを含む
-    # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-    resources :post_images, only: [:new, :create, :index, :show] #(画像投稿に)必要なルウーティングのみ生成
+    resources :post_images, only: [:new, :create, :index, :show] do #(画像投稿に)必要なルウーティングのみ生成
+      resources :post_comments, only: [:create, :destroy] #投稿画像に対してのコメントだから、post_commentsは、post_imagesと親子関係。
+    end
 end
