cef2fefaae697f5363d333312d0d99eee3d93b32
AikaOnimura <aika.onimura@gmail.com>
2020-09-28 06:21:11 +0000

    [Add]4-3 画像の投稿画面の追加

---
+++ app/controllers/post_images_controller.rb
@@ -0,0 +1,14 @@
+class PostImagesController < ApplicationController #画像投稿用のコントローラ
+  
+    def new
+    end
+
+    def create #保存
+    end
+
+    def index
+    end
+
+    def show
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  root 'post_images#index' #ログイン認証が成功後、投稿画像一覧画面が表示されるように設定
-  devise_for :users #deviseを使用する際にURLとしてusersを含む
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+    root 'post_images#index' #ログイン認証が成功後、投稿画像一覧画面が表示されるように設定
+    devise_for :users #deviseを使用する際にURLとしてusersを含む
+    # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+    resources :post_images, only: [:new, :create, :index, :show] #(画像投稿に)必要なルウーティングのみ生成
 end
