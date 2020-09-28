ea0551b92d0ecaf811905559e71c6990f1355f44
AikaOnimura <aika.onimura@gmail.com>
2020-09-28 04:22:06 +0000

    [Add]4-2 deviseを利用するためのコードをapplication_controller.rbに追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  #devise利用の機能(ユーザ登録/ログイン認証等)が使われる場合、その前にconfigure_permitted_parametersが実行される
+
+  protected
+
+  def configure_permitted_parameters #devise_parameter_sanitizer.permitでnameのデータ操作を許可するアクションメソッドが指定されている
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
 end
