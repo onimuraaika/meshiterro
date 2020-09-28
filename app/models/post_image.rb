26615cee2882ea24f0df3bdcfc0f39c6a73ba996
AikaOnimura <aika.onimura@gmail.com>
2020-09-28 14:50:13 +0000

    [Add]4-4 モデル同士の関連づけ設定を追加

--- app/models/post_image.rb
+++ app/models/post_image.rb
@@ -1,4 +1,5 @@
 class PostImage < ApplicationRecord
     belongs_to :user #PostImageモデルに関連付けられるのは、1つのUserモデル
-    attachment :image #_idは含めないも
+    attachment :image #_idは含めない
+    has_many :post_comments, dependent: :destroy #投稿画像が削除されたら、コメントも削除される設定。
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,8 @@ class User < ApplicationRecord #deviseで利用する機能が記述されてい
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
     devise :database_authenticatable, :registerable, #「:」の後ろがdeviseの機能名
            :recoverable, :rememberable, :validatable
-         
+
+    #「dependent: :destroy」は、has_manyで使えるオプション
     has_many :post_images, dependent: :destroy #has_manyの後は、1:Nの関係になるモデル名を複数形で記述
+    has_many :post_comments, dependent: :destroy #Userのデータを削除したら、Userが投稿したコメントデータも削除される設定。
 end
