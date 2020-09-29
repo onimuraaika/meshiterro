b4f2a7e8908f141011ec65323da1f684f3a4521b
AikaOnimura <aika.onimura@gmail.com>
2020-09-29 03:08:47 +0000

    [Add]4-4 詳細画面にコメント投稿するためにcreateアクションを追加

--- app/controllers/post_comments_controller.rb
+++ app/controllers/post_comments_controller.rb
@@ -2,8 +2,19 @@
 class PostCommentsController < ApplicationController
   
     def create #コメント投稿を保存
+        post_image = PostImage.find(params[:post_image_id])
+        comment = current_user.post_comments.new(post_comment_params) #comment = PostComment.new(post_comment_params)とcomment.user_id = current_user.idの2行を省略
+        comment.post_image_id = post_image.id
+        comment.save
+        redirect_to post_image_path(post_image)
     end
 
     def destroy #コメント投稿を削除
+    end
+    
+    private #コメント投稿データのストロングパラメータ設定
+    
+    def post_comment_params
+        params.require(:post_comment).permit(:comment)
     end
 end
