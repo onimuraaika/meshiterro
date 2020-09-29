30d049e05b0016c97f7854eee6224657ee21703c
AikaOnimura <aika.onimura@gmail.com>
2020-09-29 12:29:03 +0000

    [Add]4-6 プロフィール編集機能の追加/画像を保存するカラムの追加、refileで画像投稿できるようにする

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,5 @@ class User < ApplicationRecord #deviseで利用する機能が記述されてい
     has_many :post_images, dependent: :destroy #has_manyの後は、1:Nの関係になるモデル名を複数形で記述
     has_many :post_comments, dependent: :destroy #Userのデータを削除したら、Userが投稿したコメントデータも削除される設定。
     has_many :favorites, dependent: :destroy #Userがデータを消したら、も削除される設定。
+    attachment :profile_image #refileを使うにはこのメソッドが必要。書き方は、カラム名（profile_image_id）から_idを除いた名前（profile_image）を。
 end
